279e7b9fa7169da0322e165d4de8c83e2f7b2c7f
Irma Mejia <ijanettmejia@outlook.com>
2020-08-05 00:45:10 -0400

    Rewrite if statement

--- leap.js
+++ leap.js
@@ -4,8 +4,8 @@
 //
 
 export const isLeap = (year) => {
-  if (year % 4 === 0) {
-    if (year % 100 !== 0 && year % 400 === 0) return true;
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+    return true;
   } else {
     return false;
   }
